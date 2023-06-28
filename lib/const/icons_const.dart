81bf575da2f32c3b9f499849aee3bdd456e78f77
sndpsdhsvm <sandeep.sadhasivam@gmail.com>
2023-06-28 20:49:14 +0530

    2nd commit

--- lib/const/icons_const.dart
+++ lib/const/icons_const.dart
@@ -2,4 +2,5 @@ class AppIcons {
   static const leftArrow = "assets/icons/Arrow - Left 2.svg";
   static const dots = "assets/icons/Dots.svg";
   static const search = "assets/icons/Search.svg";
+  static const filter = "assets/icons/Filter.svg";
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,67 +8,113 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        leading: Container(
-          margin: const EdgeInsets.all(10),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: const Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: SvgPicture.asset(
-            AppIcons.leftArrow,
-            height: 20,
-            width: 20,
-          ),
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              debugPrint('tapped');
-            },
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 37,
-              decoration: BoxDecoration(
-                color: const Color(0xffF7F8F8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: SvgPicture.asset(
-                AppIcons.dots,
-                height: 20,
-                width: 20,
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(30, 20, 0, 40),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0,
+                )
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: SvgPicture.asset(
+                    AppIcons.search,
+                    width: 20,
+                  ),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const SizedBox(
+                        height: 20,
+                        child: VerticalDivider(
+                          thickness: 0.1,
+                          indent: 0,
+                          endIndent: 0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SvgPicture.asset(
+                        AppIcons.filter,
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                ),
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
               ),
             ),
           ),
         ],
       ),
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        child: TextField(
-          decoration: InputDecoration(
-              prefixIcon: Padding(
-                padding: const EdgeInsets.all(5),
-                child: SvgPicture.asset(
-                  AppIcons.search,
-                  width: 20,
-                ),
-              ),
-              border: const OutlineInputBorder()),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
       ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: const Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SvgPicture.asset(
+          AppIcons.leftArrow,
+          height: 20,
+          width: 20,
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+            debugPrint('tapped');
+          },
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              AppIcons.dots,
+              height: 20,
+              width: 20,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
